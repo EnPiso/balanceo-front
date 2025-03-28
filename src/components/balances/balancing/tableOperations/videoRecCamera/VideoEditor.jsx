2e7d5f86c1695358ef228b58bed060b368e5c6e8
musicprogram <jbestrial@hotmail.com>
2025-03-28 12:37:01 -0500

    loading edit videos rec

--- src/components/balances/balancing/tableOperations/videoRecCamera/VideoEditor.jsx
+++ src/components/balances/balancing/tableOperations/videoRecCamera/VideoEditor.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Slider } from "@nextui-org/react";
-import { FaClock, FaCut, FaHandScissors, FaRecordVinyl, FaRegClock, FaSave } from "react-icons/fa";
-import MyCustomButton from "../../../../../ui/MyCustomButton";
-import { ConfirmOpen } from "../../sidebarForm/ConfirmOpers";
+import { FaRecordVinyl } from "react-icons/fa";
 import VideoRecSave from "./VideoRecSave";
 import { ModalVideoRecConfirm } from "./ModalVideoRecConfirm";
 
@@ -20,15 +18,18 @@ const VideoEditor = ({
   operation
  }) => {
   const [trimmedVideoBlob, setTrimmedVideoBlob] = useState(null);
+  const [isTrimming, setIsTrimming] = useState(false); // Estado para el cargador
 
   const videoRef = useRef(null);
 
   const maxDuration = videoDuration && !isNaN(videoDuration) ? parseFloat(videoDuration) : 0;
 
-  const [isOpenConfirm,setIsOpenConfirm] = useState(false)
+  const [isOpenConfirm, setIsOpenConfirm] = useState(false);
 
   const trimVideo = () => {
     if (!videoBlob) return;
+  
+    setIsTrimming(true); // Mostrar el cargador
   
     const videoElement = document.createElement("video");
     videoElement.src = URL.createObjectURL(videoBlob);
@@ -39,6 +40,7 @@ const VideoEditor = ({
       // Validar los tiempos antes de recortar
       if (startTime >= endTime || startTime < 0 || endTime > duration) {
         alert("Tiempos inválidos para recortar el video");
+        setIsTrimming(false); // Ocultar el cargador
         return;
       }
   
@@ -56,6 +58,7 @@ const VideoEditor = ({
       recorder.onstop = () => {
         const trimmedBlob = new Blob(chunks, { type: "video/webm" });
         setTrimmedVideoBlob(trimmedBlob);
+        setIsTrimming(false); // Ocultar el cargador
       };
   
       // Ajustar el tiempo de reproducción
@@ -63,7 +66,7 @@ const VideoEditor = ({
       videoElement.play();
   
       // Ajustar el tiempo de grabación para compensar el desfase
-      const adjustedEndTime = (endTime - startTime) * 1000 - 50; // Ajuste de 50ms
+      const adjustedEndTime = (endTime - startTime) * 1000;
   
       setTimeout(() => {
         recorder.stop();
@@ -80,113 +83,94 @@ const VideoEditor = ({
     }
   }, [startTime]);
 
-
   return (
     <div>
-    {/* Video Original */}
-
-   
-
-
-
-    {/* Video Cortado */}
-    {trimmedVideoBlob ? (
-      <div>
-        <div className="flex-1 flex items-center justify-center w-full">
-          <video
-            src={URL.createObjectURL(trimmedVideoBlob)}
-            controls
-            className="w-full h-full max-h-[70%] object-contain"
-          />
+      {/* Mostrar el cargador mientras se recorta el video */}
+      {isTrimming && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="text-white text-lg font-bold">Recortando video, por favor espera...</div>
         </div>
-      </div>
-    ): (
-      <>
-        {videoBlob && (
+      )}
+
+      {/* Video Original */}
+      {trimmedVideoBlob ? (
+        <div>
           <div className="flex-1 flex items-center justify-center w-full">
             <video
               loop
               autoPlay={true}
-              ref={videoRef}
-              src={URL.createObjectURL(videoBlob)}
+              src={URL.createObjectURL(trimmedVideoBlob)}
               controls
               className="w-full h-full max-h-[70%] object-contain"
             />
-
           </div>
-        )}
-
-    </>
-    
-        
-
-    )}
-
-    {/* Controles de Recorte */}
- 
-
-     
-    {
-          videoDuration && (
-            <>
-        
-              <div className="w-full mt-4 flex flex-col items-center bg-zinc-100 py-6">
-          
-              <Slider
-              
-                fillOffset={0}
-                radius="full"
-                label={"Duración del Video"}
-                color="foreground"
-                step={0.1}
-                minValue={0}
-                maxValue={maxDuration}
-                defaultValue={[0, maxDuration]}
-                onChange={(e) => handleSliderChange(e)}
-                onChangeEnd={() => trimVideo()} // Detecta cuando el usuario suelta el slider
-                className="max-w-md text-secondary_two font-black  uppercase"
-                formatOptions={{
-                  style: "unit",
-                  unit: "second",
-                  unitDisplay: "narrow", // Opcional: muestra "s" en lugar de "seconds"
-                }}
-                
+        </div>
+      ) : (
+        <>
+          {videoBlob && (
+            <div className="flex-1 flex items-center justify-center w-full">
+              <video
+                loop
+                autoPlay={true}
+                ref={videoRef}
+                src={URL.createObjectURL(videoBlob)}
+                controls
+                className="w-full h-full max-h-[70%] object-contain"
               />
-              </div>
-            </>
-          )
-        }
+            </div>
+          )}
+        </>
+      )}
 
-    <div className="flex justify-center items-center py-4 z-50">
-      <button
-      onClick={()=> {
-        setIsOpenConfirm(true)
-      }}
-        className="py-2 px-4 rounded"
-      >
-        <FaRecordVinyl size={44} className="text-red-500"/>
-        
-      </button>
-      <VideoRecSave 
-        operation={operation}
-        videoBlob={videoBlob}
+      {/* Controles de Recorte */}
+      {videoDuration && (
+        <>
+          <div className="w-full mt-4 flex flex-col items-center bg-zinc-100 py-6">
+            <Slider
+              fillOffset={0}
+              radius="full"
+              label={"Duración del Video"}
+              color="foreground"
+              step={0.1}
+              minValue={0}
+              maxValue={maxDuration}
+              defaultValue={[0, maxDuration]}
+              onChange={(e) => handleSliderChange(e)}
+              onChangeEnd={() => trimVideo()} // Detecta cuando el usuario suelta el slider
+              className="max-w-md text-secondary_two font-black uppercase"
+              formatOptions={{
+                style: "unit",
+                unit: "second",
+                unitDisplay: "narrow", // Opcional: muestra "s" en lugar de "seconds"
+              }}
+            />
+          </div>
+        </>
+      )}
+
+      <div className="flex justify-center items-center py-4 z-50">
+        <button
+          onClick={() => {
+            setIsOpenConfirm(true);
+          }}
+          className="py-2 px-4 rounded"
+        >
+          <FaRecordVinyl size={44} className="text-red-500" />
+        </button>
+        <VideoRecSave operation={operation} videoBlob={videoBlob} />
+      </div>
+
+      <ModalVideoRecConfirm
+        isOpen={isOpenConfirm}
+        setIsOpen={setIsOpenConfirm}
+        handleSave={() => {
+          setVideoBlob(null);
+          setVideoDuration(null);
+        }}
+        title="¿Quieres eliminar Vídeo,"
+        description={"para grabar y reemplazar?"}
       />
-      
     </div>
-
-    <ModalVideoRecConfirm
-      isOpen={isOpenConfirm}
-      setIsOpen={setIsOpenConfirm}
-      handleSave={() => {
-        setVideoBlob(null)
-        setVideoDuration(null)
-      }}
-      title="¿Quieres eliminar Vídeo,"
-      description={"para grabar y reemplazar?"}
-    />
-        
-
-  </div>
   );
 };
 
